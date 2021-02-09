2aa1aaa3a385dc39fb03c983772a027342478ffd
masahitoooki <58727760+masahitoooki@users.noreply.github.com>
2021-02-09 12:57:32 +0900

    initial

---
+++ demo/service/UserService.java
@@ -0,0 +1,93 @@
+package com.example.demo.service;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.dto.UserRequest;
+import com.example.demo.dto.UserSearchRequest;
+import com.example.demo.dto.UserUpdateRequest;
+import com.example.demo.entity.User;
+import com.example.demo.repository.UserMapper;
+import com.example.demo.repository.UserRepository;
+/**
+ * ユーザー情報 Service
+ */
+@Service
+@Transactional(rollbackOn = Exception.class)
+public class UserService {
+    /**
+     * ユーザー情報 Repository
+     */
+    @Autowired
+    private UserRepository userRepository;
+
+    /**
+     * ユーザー情報 全検索
+     * @return 検索結果
+     */
+    public List<User> searchAll() {
+      return userRepository.findAll();
+    }
+
+    /**
+     * ユーザー情報 主キー検索
+     * @return 検索結果
+     */
+    public User findById(Long id) {
+        return userRepository.findById(id).get();
+    }
+    /**
+     * ユーザー情報 更新
+     * @param user ユーザー情報
+     */
+    public void update(UserUpdateRequest userUpdateRequest) {
+        User user = findById(userUpdateRequest.getId());
+        user.setAddress(userUpdateRequest.getAddress());
+        user.setName(userUpdateRequest.getName());
+        user.setPhone(userUpdateRequest.getPhone());
+        user.setUpdateDate(new Date());
+        userRepository.save(user);
+    }
+
+    /**
+     * ユーザー情報 新規登録
+     * @param user ユーザー情報
+     */
+    public void create(UserRequest userRequest) {
+      Date now = new Date();
+      User user = new User();
+      user.setName(userRequest.getName());
+      user.setAddress(userRequest.getAddress());
+      user.setPhone(userRequest.getPhone());
+      user.setCreateDate(now);
+      user.setUpdateDate(now);
+      userRepository.save(user);
+    }
+
+    /**
+    * ユーザー情報 物理削除
+    * @param id ユーザーID
+    */
+   public void delete(Long id) {
+       User user = findById(id);
+       userRepository.delete(user);
+   }
+
+   /**
+    * ユーザー情報 Mapper
+    */
+   @Autowired
+   private UserMapper userMapper;
+   /**
+    * ユーザー情報検索
+    * @return 検索結果
+    */
+   public User search(UserSearchRequest userSearchRequest) {
+     return userMapper.search(userSearchRequest);
+   }
+}
